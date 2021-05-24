74bae1b951acc7b5877ceb0f55f468f2a7d722aa
Bruno Gabriel <celilvr13@gmail.com>
2021-05-24 04:04:09 -0300

    executavel on!!

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 import sys
-from cx_Freeze import setup, executable
+from cx_Freeze import setup, Executable
+from PyQt5 import uic,QtWidgets
+import sqlite3
+from PyQt5.QtWidgets import QMessageBox
 
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
 executables = [
-    executable("__main__.py",base=base)
+    Executable("__main__.py",base=base)
 ]
 
 setup(
     name="Calculaddora Câmbio",
     version="1.0",
     description="App que calcula o câmbio",
-    executables=executable
+    executables=Executable
 )
